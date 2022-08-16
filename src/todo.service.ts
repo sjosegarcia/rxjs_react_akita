ee88839ff5964cd61853a722c70c8f5b9338d884
Steven Garcia <sjosegarcia23@gmail.com>
2022-08-16 02:35:56 -0400

    react rxjs example

---
+++ src/todo.service.ts
@@ -0,0 +1,30 @@
+import { ID } from "@datorama/akita";
+import { VISIBILITY_FILTER, createTodo } from "./todo.model";
+import { TodosStore, todosStore } from "./todo.store";
+
+export class TodosService {
+  constructor(private todosStore: TodosStore) {}
+
+  updateFilter(filter: VISIBILITY_FILTER) {
+    this.todosStore.update({
+      ui: {
+        filter
+      }
+    });
+  }
+
+  complete(id: ID) {
+    todosStore.update(id, entity => ({ completed: !entity.completed }));
+  }
+
+  add(text: string) {
+    const todo = createTodo(text);
+    this.todosStore.add(todo);
+  }
+
+  delete(id: ID) {
+    this.todosStore.remove(id);
+  }
+}
+
+export const todosService = new TodosService(todosStore);
